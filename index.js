7f2c8a79a18bab3592066723df40855aa6375440
Tsurematsu <daniel120802@gmail.com>
2024-06-15 03:41:13 -0500

    actualizacion

--- index.js
+++ index.js
@@ -5,11 +5,13 @@
 // saveDataToIndexedDB(databases.MyDatabaseActionevent, tuobjetoquetienedatospaguardar);
 
 import modal from './modal/modal.js';
+const urlBase = (url)=> import.meta.url.split('/').slice(0, -1).join('/') + "/" + url.replace('//', '');
 const main = async function(){
-    await modal.LoadModal();
-    modal.Modales[0].properties.fillForm({
+    const modal1 = new modal({});
+    await modal1.LoadModal();
+    const formulario = modal1.Modales[0].properties.fillForm({
         titulo: 'Hola mundo',
-        name: 'juan',
+        name: 'juan camilo',
         email: 'fakeEmail@gmail.com',
         kaka: false,
         selection: {options:['hola', 'gato', 'perro'], selected: 0},
@@ -17,28 +19,8 @@ const main = async function(){
         aceptar: ()=>{console.log("aceptar botón");},
         cancelar: ()=>{console.log("cancelar botón");},
     })
-
-    
-
+    formulario.click(()=>{
+        console.log(formulario.email);
+    }).email
 }
 main();
-
-
-// import tab5Action from "./tab5-action/tab5-action.js";
-// tab5Action({
-//     elementContainer: document.getElementById('tab5-action'), 
-//     files:[
-//         {name: 'Image 1', path: 'https://imagen 1'},
-//         {name: 'Image 2', path: 'https://imagen 2'},
-//         {name: 'Image 3', path: 'https://imagen 3'},
-//     ],
-//     onSave: (datos)=>{
-//         console.log('onSave: ', datos);
-//     },
-//     onCancel: ()=>{
-        
-//     },
-// }).then((objectModal)=>{
-//     document.getElementById('open').addEventListener('click', objectModal.open);
-//     document.getElementById('close').addEventListener('click', objectModal.close);
-// });
--- modal/modal.js
+++ modal/modal.js
@@ -1,27 +1,34 @@
-const config = {
-    classContainer:"modal",
-    styles:"./modal.css",
-    html:"./modal.html",
-    separador:"_",
-}
-const urlBase = (url)=> import.meta.url.split('/').slice(0, -1).join('/') + "/" + url.replace('//', '');
+const urlBase = (url)=> url;
 const getArchive = async (url)=> await fetch(urlBase(url)).then((response)=>response.text()); 
-export default new function(){
+export default function({html="./modal.html", separador="_", classContainer="modal"}){
     this.Modales = [];
     this.ultimoModal = null;
     this.LoadModal = ()=> new Promise(async (resolve, _)=>{
-        for (const item in {...document.querySelectorAll(`.${config.classContainer}`)}) {
-            const element = document.querySelectorAll(`.${config.classContainer}`)[item];
-            this.class = `${config.classContainer}_${Math.random().toString(36).substring(7)}`;
-            const HTMLmodal = await getArchive(config.html);
-            const TEXTstyles = await getArchive(config.styles);
+        for (const item in {...document.querySelectorAll(`.${classContainer}`)}) {
+            const element = document.querySelectorAll(`.${classContainer}`)[item];
+            this.class = `${classContainer}_${Math.random().toString(36).substring(7)}`;
+            let HTMLmodal = await getArchive(html);
+            const Estilos = [];
+            HTMLmodal.split('<link').forEach((item, index)=>{
+                const center = item.split('>')[0]
+                if (center.includes('stylesheet')) {
+                    const url = center.split('href="')[1].split('"')[0];
+                    Estilos.push(url);
+                    HTMLmodal = HTMLmodal.replace(`<link${center}>`, '');
+                }
+            })
+            HTMLmodal = HTMLmodal.trim();
             const contenedor = document.createElement('div');
-            const estilos = document.createElement('style');
-            estilos.innerHTML = `.${this.class}{\n ${TEXTstyles}\n}`;
             contenedor.innerHTML = HTMLmodal;
             contenedor.classList.add(this.class);
             element.appendChild(contenedor);
-            element.appendChild(estilos);
+            const estilos = null;
+            for (let index = 0; index < Estilos.length; index++) {
+                const TEXTstyles = await getArchive(Estilos[index]);
+                const estilos = document.createElement('style');
+                estilos.innerHTML = `.${this.class}{\n ${TEXTstyles}    \n}`;
+                element.appendChild(estilos);
+            }
             this.Modales.push({
                 element,
                 contenedor,
@@ -34,27 +41,32 @@ export default new function(){
     });
     function Modal({element, ElementModal, elementStyle}){
         this.fillForm = (formObject)=>{
+            const inputsList = {};
             Object.entries(formObject).forEach(([element, value]) => {
-                const input = ElementModal.querySelector(`*[name="${element.split(config.separador)[0]}"]`);
+                const input = ElementModal.querySelector(`*[name="${element.split(separador)[0]}"]`);
                 if (input!==null) {
-                    // console.log('input -> ', input.type, ' -> ', input, ' -> ', value);
                     if (input.type == 'file') {
                         input.files = value;
+                        inputsList[element]=input;
                     }else if (input.type == 'checkbox') {
                         input.checked = value;
+                        inputsList[element]=input;
                     }else if (input.type == 'radio') {
                         input.innerHTML = '';
                         value.options.forEach((item, index)=>{
                             const radio = document.createElement('input');
                             const label = document.createElement('label');
-                            const idLabel = `${element.split(config.separador)[0]}_${index}_${Math.random().toString(36).substring(7)}`;
+                            const idLabel = `${element.split(separador)[0]}_${index}_${
+                                                Math.random().toString(36).substring(7)}`;
                             radio.type = 'radio';
                             radio.name = element;
                             radio.value = item;
                             radio.id = idLabel;
                             label.innerHTML = item;
                             label.htmlFor = idLabel;
-                            if (item == value.selected) {radio.checked = true;} else if (index == value.selected) {radio.checked = true;}
+                            inputsList[element]=radio;
+                            if (item == value.selected) {radio.checked = true;} 
+                            else if (index == value.selected) {radio.checked = true;}
                             input.parentNode.insertBefore(label, input.nextSibling);
                             input.parentNode.insertBefore(radio, input.nextSibling);
                         });
@@ -65,18 +77,65 @@ export default new function(){
                             const option = document.createElement('option');
                             option.value = item;
                             option.innerHTML = item;
-                            if (item == value.selected) {option.selected = true;} else if (index == value.selected) {option.selected = true;}
+                            if (item == value.selected) {option.selected = true;} 
+                            else if (index == value.selected) {option.selected = true;}
                             input.appendChild(option);
                         });
+                        inputsList[element]=input;
                     }else if(input.type == undefined) {
                         input.innerHTML = value;
+                        inputsList[element]=input;
                     }else if(input.type == 'text') {
                         input.value = value;
+                        inputsList[element]=input;
                     }else if(input.type == 'button') {
                         input.addEventListener('click', value);
+                        inputsList[element]=input;
+                    }
+                }else if(value.type!==undefined){
+                    if (value.type == 'text') {
+                        const input = document.createElement('input');
+                        input.type = 'text';
+                        input.name = element;
+                        input.value = value.value;
+                        ElementModal.appendChild(input);
+                        inputsList.push({element, element:input});
                     }
                 }
-
+            });
+            return new Proxy(formObject, {
+                get: function(_, prop, receiver) {
+                    const eventosExistentes = [
+                        'click', 'change', 'input', 'keyup', 
+                        'keydown', 'keypress', 'focus', 
+                        'blur', 'change', 'select', 
+                        'submit', 'reset'
+                    ];
+                    const retornoCall = (callback, evento)=>new Proxy({}, {
+                        get: function(_, prop1, receiver) {
+                            const input = inputsList[prop1];
+                            input.addEventListener(evento, (event)=>{callback(event, input)});
+                            return Reflect.get(...arguments);
+                        },
+                    });
+                    let eventoObtenido = null;
+                    eventosExistentes.forEach((evento)=>{
+                        if (prop==evento) eventoObtenido = (callback)=>{return retornoCall(callback, evento)};
+                    })
+                    if (eventoObtenido!==null) return eventoObtenido;
+                    const input = inputsList[prop]
+                    if (input.type == 'text') {
+                        return input.value;
+                    }
+                    return Reflect.get(...arguments);
+                },
+                set: function(_, prop, value, receiver) {
+                    const input = inputsList[prop];
+                    if (input.type == 'text') {
+                        input.value = value;
+                    }
+                    return Reflect.set(...arguments);
+                }
             });
         }
     }
